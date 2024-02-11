b62738852a9d963045e6c0c910b6b4ab60e9a920
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-02-11 04:33:50 +0100

    Add missing localization for CurrencyInput in SavingsSettings.vue

--- src/components/SavingsSettings.vue
+++ src/components/SavingsSettings.vue
@@ -7,7 +7,7 @@
     </v-row>
     <v-row class="mt-16">
       <v-col cols="12" sm="6">
-        <CurrencyInput label="Sparbetrag" v-model="saveAmount" />
+        <CurrencyInput :label="$t('savings-amount2')" v-model="saveAmount" />
       </v-col>
     </v-row>
     <v-row>
