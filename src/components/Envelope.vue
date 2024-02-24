b3aa2cd0e65004c0e85b71ea30bab55ca31484d1
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-02-24 03:04:21 +0100

    Clean up Envelope.vue and OverflowEnvelope.vue

--- src/components/Envelope.vue
+++ src/components/Envelope.vue
@@ -3,28 +3,29 @@
     @click="envelopeClicked"
     variant="tonal"
     min-height="180"
-    class="d-flex align-center justify-center"
+    class="d-flex align-center justify-center text-center"
   >
-    <div class="text-center">
-      <v-card-item>
-        <v-card-title>{{ title }}</v-card-title>
-        <v-card-subtitle>{{ $t('priority') }} {{ priority }}</v-card-subtitle>
-      </v-card-item>
-      <v-card-text>
-        <div>
-          {{
-            !noLimit
-              ? $t('goal') + formatCurrency(goal)
-              : $t('goal') + $t('no-limit')
-          }}
-        </div>
-
-        <div class="font-weight-bold">
-          {{ formatCurrency(currentAmount) }}
-        </div>
-        <div>+{{ formatCurrency(increment) }}</div>
-      </v-card-text>
-    </div>
+    <v-row>
+      <v-col>
+        <v-card-item>
+          <v-card-title>{{ title }}</v-card-title>
+          <v-card-subtitle>{{ $t('priority') }} {{ priority }}</v-card-subtitle>
+        </v-card-item>
+        <v-card-text>
+          <p>
+            {{
+              !noLimit
+                ? $t('goal') + formatCurrency(goal)
+                : $t('goal') + $t('no-limit')
+            }}
+          </p>
+          <p class="font-weight-bold">
+            {{ formatCurrency(currentAmount) }}
+          </p>
+          <p>+{{ formatCurrency(increment) }}</p>
+        </v-card-text>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
--- src/components/OverflowEnvelope.vue
+++ src/components/OverflowEnvelope.vue
@@ -3,16 +3,18 @@
     @click="OverflowEnvelopeClicked"
     variant="tonal"
     min-height="180"
-    class="d-flex align-center justify-center"
+    class="d-flex align-center justify-center text-center"
   >
-    <div class="text-center">
-      <v-card-item>
-        <v-card-title>{{ $t('overflow-envelope') }}</v-card-title>
-      </v-card-item>
-      <v-card-text class="font-weight-bold">
-        {{ formatCurrency(currentAmount) }}
-      </v-card-text>
-    </div>
+    <v-row>
+      <v-col>
+        <v-card-item>
+          <v-card-title>{{ $t('overflow-envelope') }}</v-card-title>
+        </v-card-item>
+        <v-card-text class="font-weight-bold">
+          {{ formatCurrency(currentAmount) }}
+        </v-card-text>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
