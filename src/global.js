52a0a4ad85ad6ff8a07570da4e911833abe725db
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-03-01 08:05:49 +0100

    Add findMoneyboxById() to global.js

--- src/global.js
+++ src/global.js
@@ -3,25 +3,41 @@
 import { reactive, readonly } from 'vue'
 
 const moneyboxes = reactive([])
+const moneyboxesMap = new Map()
 const moneyboxesLoaded = false
 
 function setMoneyboxes(newMoneyboxes) {
   moneyboxes.splice(0, moneyboxes.length, ...newMoneyboxes)
+  moneyboxesMap.clear()
+  newMoneyboxes.forEach((moneybox) => {
+    moneyboxesMap.set(moneybox.id, moneybox)
+  })
 }
 
 function addMoneybox(newMoneybox) {
   moneyboxes.push(newMoneybox)
+  moneyboxesMap.set(newMoneybox.id, newMoneybox)
 }
 
-function deleteMoneybox(Moneybox) {
-  const index = moneyboxes.findIndex((moneybox) => moneybox.id === Moneybox.id)
-  moneyboxes.splice(index, 1)
+function deleteMoneybox(moneyboxToDelete) {
+  const index = moneyboxes.findIndex(
+    (moneybox) => moneybox.id === moneyboxToDelete.id
+  )
+  if (index !== -1) {
+    moneyboxes.splice(index, 1)
+    moneyboxesMap.delete(moneyboxToDelete.id)
+  }
+}
+
+function findMoneyboxById(id) {
+  return moneyboxesMap.get(id)
 }
 
 export default {
   moneyboxes: readonly(moneyboxes),
-  moneyboxesLoaded: moneyboxesLoaded,
+  moneyboxesLoaded,
   addMoneybox,
   deleteMoneybox,
-  setMoneyboxes
+  setMoneyboxes,
+  findMoneyboxById
 }
