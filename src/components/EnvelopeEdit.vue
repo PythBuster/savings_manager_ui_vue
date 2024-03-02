c9d88d945ed2f0d011a05d772413ec5d2adefbd6
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-03-02 10:33:09 +0100

    Add error dialog to updateMoneybox() in EnvelopeEdit.vue

--- src/components/EnvelopeEdit.vue
+++ src/components/EnvelopeEdit.vue
@@ -23,27 +23,60 @@
         >
       </v-col>
     </v-row>
+    <ErrorDialog
+      v-model="showErrorDialog"
+      :error-message="errorMessage"
+      @update:modelValue="showErrorDialog = $event"
+    ></ErrorDialog>
   </v-container>
 </template>
 <script setup>
+import { ref } from 'vue'
 import router from '@/router'
 import global from '@/global.js'
-import { ref } from 'vue'
 import { updateMoneybox } from '@/api.js'
+import { useI18n } from 'vue-i18n'
+import { APIError } from '@/customerrors'
+
+// t used for error dialog, otherwise $t globally available
+const { t } = useI18n({})
 
 const props = defineProps({
   id: Number
 })
+
+const showErrorDialog = ref(false)
+const errorMessage = ref('')
 
 const newTitle = ref(global.findMoneyboxById(props.id).name)
 const newTargetAmount = ref(global.findMoneyboxById(props.id).goal)
 const newSaveAmount = ref(global.findMoneyboxById(props.id).increment)
 
 async function saveClicked() {
-  await updateMoneybox(global.findMoneyboxById(props.id), newTitle.value)
-
-  router.push({
-    path: `/envelope/${props.id}`
-  })
+  try {
+    await updateMoneybox(global.findMoneyboxById(props.id), newTitle.value)
+    router.push({
+      path: `/envelope/${props.id}`
+    })
+  } catch (error) {
+    if (error instanceof APIError) {
+      if (error.status === 404) {
+        errorMessage.value = t('error-not-found', {
+          name: newTitle.value
+        })
+      } else if (error.status === 405) {
+        errorMessage.value = t('error-duplicate-name', {
+          name: newTitle.value
+        })
+      } else if (error.status === 422) {
+        errorMessage.value = t('error-must-be-string')
+      } else if (error.status === 500) {
+        errorMessage.value = error.message
+      }
+    } else {
+      errorMessage.value = error
+    }
+    showErrorDialog.value = true
+  }
 }
 </script>
