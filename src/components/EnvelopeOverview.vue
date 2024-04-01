fc0c67c9f87e93f8d57c6a4a2a08f36e6b1b8b46
PythBuster <pythbuster@gmail.com>
2024-04-01 22:12:56 +0200

    issue #1 feat: add back button to EnvelopeOverview page

--- src/components/EnvelopeOverview.vue
+++ src/components/EnvelopeOverview.vue
@@ -1,110 +1,119 @@
 <template>
   <v-container>
-    <v-row v-if="global.findMoneyboxById(id)">
-      <v-col cols="auto" md="auto">
-        <h1 class="text-h4">
-          {{ $t('envelope') + ': ' + global.findMoneyboxById(id).name }}
-        </h1>
-      </v-col>
-    </v-row>
-    <v-row v-if="global.findMoneyboxById(id)" justify="space-between">
-      <v-col cols="auto">
-        <v-table>
-          <tbody>
-            <tr>
-              <td>{{ $t('balance') }}</td>
-              <td>{{ formatCurrency(global.findMoneyboxById(id).balance) }}</td>
-            </tr>
-            <tr>
-              <td>{{ $t('goal-amount') }}</td>
-              <td>
-                {{
-                  !global.findMoneyboxById(id).noLimit
-                    ? formatCurrency(global.findMoneyboxById(id).goal)
-                    : $t('no-limit')
-                }}
-              </td>
-            </tr>
-            <tr>
-              <td>{{ $t('savings-amount') }}:</td>
-              <td>
-                {{ formatCurrency(global.findMoneyboxById(id).increment) }}
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-col>
-      <v-col cols="auto" class="d-flex flex-column">
-        <v-btn @click="handleTransactionDialog('deposit')" class="mb-2">{{
-          $t('deposit')
-        }}</v-btn>
-        <v-btn @click="handleTransactionDialog('withdraw')" class="mb-2">{{
-          $t('withdraw')
-        }}</v-btn>
-        <v-btn @click="handleTransferDialog">{{ $t('transfer') }}</v-btn>
-      </v-col>
-      <v-col cols="auto" class="d-flex flex-column">
-        <v-btn @click="changeSettingsClicked" class="mb-2">{{
-          $t('change-settings')
-        }}</v-btn>
-        <v-btn @click="deleteClicked" color="warning">{{
-          $t('delete-envelope')
-        }}</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="8">
-        <LastTransactions />
-      </v-col>
-      <v-col cols="12" md="4">
-        <BarChart />
-      </v-col>
-    </v-row>
-    <v-dialog
-      v-if="global.findMoneyboxById(id)"
-      v-model="showDeleteDialog"
-      max-width="500px"
-    >
-      <v-card>
-        <v-card-title class="headline">{{
-          $t('delete-envelope')
-        }}</v-card-title>
-        <v-card-text>
-          {{ $t('delete-envelope-question-1') }}
-          <span class="font-weight-bold">{{
-            global.findMoneyboxById(id).name
-          }}</span
-          >{{ $t('delete-envelope-question-2') }}?
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" @click="showDeleteDialog = false">{{
-            $t('cancel')
+    <v-col>
+      <button 
+        type="button"    
+        @click="$router.go(-1)" class="my-5 btn btn-outline-success">&laquo; 
+        Back
+      </button>
+    </v-col>
+    <v-col cols="auto">
+      <v-row v-if="global.findMoneyboxById(id)">
+        <v-col cols="auto" md="auto">
+          <h1 class="text-h4">
+            {{ $t('envelope') + ': ' + global.findMoneyboxById(id).name }}
+          </h1>
+        </v-col>
+      </v-row>
+      <v-row v-if="global.findMoneyboxById(id)" justify="space-between">
+        <v-col cols="auto">
+          <v-table>
+            <tbody>
+              <tr>
+                <td>{{ $t('balance') }}</td>
+                <td>{{ formatCurrency(global.findMoneyboxById(id).balance) }}</td>
+              </tr>
+              <tr>
+                <td>{{ $t('goal-amount') }}</td>
+                <td>
+                  {{
+                    !global.findMoneyboxById(id).noLimit
+                      ? formatCurrency(global.findMoneyboxById(id).goal)
+                      : $t('no-limit')
+                  }}
+                </td>
+              </tr>
+              <tr>
+                <td>{{ $t('savings-amount') }}:</td>
+                <td>
+                  {{ formatCurrency(global.findMoneyboxById(id).increment) }}
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+        <v-col cols="auto" class="d-flex flex-column">
+          <v-btn @click="handleTransactionDialog('deposit')" class="mb-2">{{
+            $t('deposit')
           }}</v-btn>
-          <v-btn color="red darken-1" @click="handleDeleteConfirm">{{
-            $t('delete')
+          <v-btn @click="handleTransactionDialog('withdraw')" class="mb-2">{{
+            $t('withdraw')
           }}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <ErrorDialog
-      v-model="showErrorDialog"
-      :error-message="errorMessage"
-      @update:modelValue="handleErrorDialogClose($event)"
-    ></ErrorDialog>
-    <TransactionDialog
-      v-model="showTransactionDialog"
-      :action="currentActionType"
-      :id="id"
-      @confirm="handleTransactionConfirm"
-      @update:modelValue="handleTransactionDialogClose($event)"
-    />
-    <TransferDialog
-      v-model="showTransferDialog"
-      :sourceId="id"
-      @confirm="handleTransferConfirm"
-      @update:modelValue="handleTransferDialogClose($event)"
-    ></TransferDialog>
+          <v-btn @click="handleTransferDialog">{{ $t('transfer') }}</v-btn>
+        </v-col>
+        <v-col cols="auto" class="d-flex flex-column">
+          <v-btn @click="changeSettingsClicked" class="mb-2">{{
+            $t('change-settings')
+          }}</v-btn>
+          <v-btn @click="deleteClicked" color="warning">{{
+            $t('delete-envelope')
+          }}</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="8">
+          <LastTransactions />
+        </v-col>
+        <v-col cols="12" md="4">
+          <BarChart />
+        </v-col>
+      </v-row>
+      <v-dialog
+        v-if="global.findMoneyboxById(id)"
+        v-model="showDeleteDialog"
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title class="headline">{{
+            $t('delete-envelope')
+          }}</v-card-title>
+          <v-card-text>
+            {{ $t('delete-envelope-question-1') }}
+            <span class="font-weight-bold">{{
+              global.findMoneyboxById(id).name
+            }}</span
+            >{{ $t('delete-envelope-question-2') }}?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" @click="showDeleteDialog = false">{{
+              $t('cancel')
+            }}</v-btn>
+            <v-btn color="red darken-1" @click="handleDeleteConfirm">{{
+              $t('delete')
+            }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <ErrorDialog
+        v-model="showErrorDialog"
+        :error-message="errorMessage"
+        @update:modelValue="handleErrorDialogClose($event)"
+      ></ErrorDialog>
+      <TransactionDialog
+        v-model="showTransactionDialog"
+        :action="currentActionType"
+        :id="id"
+        @confirm="handleTransactionConfirm"
+        @update:modelValue="handleTransactionDialogClose($event)"
+      />
+      <TransferDialog
+        v-model="showTransferDialog"
+        :sourceId="id"
+        @confirm="handleTransferConfirm"
+        @update:modelValue="handleTransferDialogClose($event)"
+      ></TransferDialog>
+    </v-col>
   </v-container>
 </template>
 <script setup>
