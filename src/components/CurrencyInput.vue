1dd5f174931f5f6c81202ad5fc7c3bb95645e2c1
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-04-05 16:10:40 +0200

    Fix position of boolean to disable autoEmit in CurrencyInput.vue (for future reference only, this breaks functionality) and add comment about warning without defined emits

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -5,15 +5,22 @@
 <script setup>
 import { useCurrencyInput } from 'vue-currency-input'
 
+// NOTE:
+// This defines an emit for update:modelValue, but not for change
+// In order to prevent warning, defineEmits('update:modelValue', 'change') would be needed
 defineModel()
 
-const { inputRef, formattedValue } = useCurrencyInput({
-  currency: 'EUR',
-  hideCurrencySymbolOnFocus: false,
-  hideGroupingSeparatorOnFocus: false,
-  precision: 2,
-  valueRange: { min: 0 },
-  locale: 'de-DE',
-  autoEmit: false // handled by defineModel()
-})
+const { inputRef, formattedValue } = useCurrencyInput(
+  {
+    currency: 'EUR',
+    hideCurrencySymbolOnFocus: false,
+    hideGroupingSeparatorOnFocus: false,
+    precision: 2,
+    valueRange: { min: 0 },
+    locale: 'de-DE'
+  },
+  // FIX: Proper position for autoEmit boolean
+  // NOTE: This makes the component non-function, for future reference of the correct position only
+  false
+)
 </script>
