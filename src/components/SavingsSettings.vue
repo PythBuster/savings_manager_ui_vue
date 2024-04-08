f40fafd68fa370288f0a41b86921ec731054c258
Jasmin68k <50185302+Jasmin68k@users.noreply.github.com>
2024-04-08 14:57:50 +0200

    Add list of options for savings cycle to SavingsSettings.vue (no functionality yet)

--- src/components/SavingsSettings.vue
+++ src/components/SavingsSettings.vue
@@ -11,6 +11,23 @@
       </v-col>
     </v-row>
     <v-row>
+      <v-col cols="12" sm="6">
+        <v-card>
+          <v-list v-model="selectedMode">
+            <v-list-item
+              v-for="(mode, index) in modes"
+              :key="index"
+              :value="mode"
+              @click="selectedMode = mode"
+              :class="{ 'v-list-item--active': selectedMode === mode }"
+            >
+              <v-list-item-title>{{ mode }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col class="d-flex justify-end">
         <v-btn
           @click="saveClicked"
@@ -23,10 +40,22 @@
 </template>
 <script setup>
 import router from '@/router/index.js'
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n({})
+
+const selectedMode = ref(t('monthly'))
+
+const modes = ref([])
 
 // Dummy data
 const saveAmount = ref(100)
+
+// Use watchEffect to automatically update when locale changes
+watchEffect(() => {
+  modes.value = [t('daily'), t('weekly'), t('monthly'), t('yearly')]
+})
 
 function saveClicked() {
   router.push({
