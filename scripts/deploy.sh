b38a96ce7c3ebc16fe106dbbf9485e8df5dd542e
PythBuster <pythbuster@gmail.com>
2024-09-08 14:34:44 +0200

    feat: add deploy script, which builds static files and copies to savings_manager BE

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+# adapt output dir, where your generated static vueJS files shall be copied in 
+DIST_DIR=/home/david/dev/python/savings_manager/static
+
+cd ..
+npm run build
+cp -r dist/* $DIST_DIR
