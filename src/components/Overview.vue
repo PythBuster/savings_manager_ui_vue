aeddb9d43aa470b5fc7849acf610d8df1426de3f
PythBuster <pythbuster@gmail.com>
2025-10-31 21:47:09 +0100

    refactor: clean up envelopes overview with safer list handling and minor code simplification

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -7,9 +7,10 @@
         </h1>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col
-        v-for="(card, index) in sortedMoneyboxes.slice(1,)"
+        v-for="(card, index) in sortedMoneyboxes.slice(1)"
         :key="card.id"
         cols="12"
         sm="6"
@@ -18,29 +19,33 @@
       >
         <Envelope :id="card.id" />
       </v-col>
+
       <v-col cols="12" sm="6" md="4" lg="3">
         <NewEnvelope />
       </v-col>
     </v-row>
+
     <v-row>
-      <v-col cols="12" sm="6"
-      :key="-1"
+      <v-col
+        v-if="sortedMoneyboxes.length"
+        cols="12"
+        sm="6"
+        :key="-1"
       >
-        <Envelope :id="sortedMoneyboxes[0].id" />      
+        <Envelope :id="sortedMoneyboxes[0].id" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 import global from '@/global.js'
 import { useDisplay } from 'vuetify'
 
-const display = ref(useDisplay())
+const display = useDisplay()
 
-const sortedMoneyboxes = computed(() => {
-  return [...global.moneyboxes].sort((a, b) => a.priority - b.priority);
-})
-
+const sortedMoneyboxes = computed(() =>
+  [...global.moneyboxes].sort((a, b) => a.priority - b.priority)
+)
 </script>
