720432d5cc408eab3ef23ef2c96a7672b17aa35f
PythBuster <pythbuster@gmail.com>
2025-11-01 00:21:22 +0100

    refactor: add consistent defaults and stack trace capture to custom error classes

--- src/customerrors.js
+++ src/customerrors.js
@@ -2,13 +2,16 @@ export class DataError extends Error {
   constructor(message = 'DataError') {
     super(message)
     this.name = 'DataError'
+    Error.captureStackTrace?.(this, DataError)
   }
 }
+
 export class APIError extends Error {
-  constructor(message, status, details = null) {
+  constructor(message = 'APIError', status = 500, details = null) {
     super(message)
     this.name = 'APIError'
     this.status = status
     this.details = details
+    Error.captureStackTrace?.(this, APIError)
   }
 }
